17ccb5a102e03f14c49053b3cd81ea11bcea0c90
InnokentiiHorikov <95141955+InnokentiiHorikov@users.noreply.github.com>
2026-08-26 15:38:03 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,7 +6,11 @@ import tqdm
 from pathlib import Path
 import h5py
 import pycolmap
+import torch
+from lightglue import ALIKED, LightGlue
+from lightglue.utils import load_image, rbd
 
+import pickle
 
 from database import *
 from h5py_file import *
@@ -15,84 +19,82 @@ from h5py_file import *
 def detect_keypoints(
     paths: list[Path],
     feature_dir: Path,
-    num_features: int = 4096,
-    resize_to: int = 1024,
-    #device: torch.device = torch.device("cpu"),
-) -> None:
-    """Detects the keypoints in a list of images with ALIKED
+    max_num_keypoints: int = 4096, 
+    detection_threshold: float = 0.01,
+    device: torch.device = 'cpu'
     
-    Stores them in feature_dir/keypoints.h5 and feature_dir/descriptors.h5
-    to be used later with LightGlue
-    """
+) -> None:
     #dtype = torch.float32 # ALIKED has issues with float16
     
-    extractor = cv2.SIFT_create()
+    extractor = ALIKED(max_num_keypoints=max_num_keypoints, 
+                           detection_threshold=detection_threshold).eval().to(device)
     
     feature_dir.mkdir(parents=True, exist_ok=True)
 
     
+    
     with h5py.File(feature_dir / "keypoints.h5", mode="w") as f_keypoints, \
          h5py.File(feature_dir / "descriptors.h5", mode="w") as f_descriptors:
+        masters_dict = {}
         
         for path in tqdm(paths, desc="Computing keypoints"):
             key = path.name
             
-            image = cv2.imread(path)
+            image = load_image(path).to(device)
             
-            kp, ds = extractor.detectAndCompute(image, None)
-            '''
-            kp = [{
-                        'pt': p.pt,
-                        'size': p.size,
-                        'angle': p.angle,
-                        'response': p.response,
-                        'octave': p.octave,
-                        'class_id': p.class_id
-                        } for p in kp]
-            '''
-            kp = np.array([kps.pt for kps in kp])
-            f_keypoints[key] = kp
-            f_descriptors[key] = ds
+            feats = extractor.extract(image)
+            kps, ds = feats['keypoints'].squeeze(), feats['descriptors'].squeeze()
 
+            kps, ds = kps.detach().cpu().numpy(), ds.detach().cpu().numpy()
+            
+            f_keypoints[key] = kps
+            f_descriptors[key] = ds
+            
+            masters_dict[key] = feats
+            
+        torch.save(masters_dict, 'features.pt')
+        
+        
+            
     
 def keypoint_distances(
     paths: list[Path],
     index_pairs: list[tuple[int, int]],
     feature_dir: Path,
     min_matches: int = 15,
-    verbose: bool = True,
+    verbose: bool = False,
+    device: torch.device = 'cpu'
     #device: torch.device = torch.device("cpu"),
 ) -> None:
 
-    matcher = cv2.BFMatcher()
     
-    with h5py.File(feature_dir / "keypoints.h5", mode="r") as f_keypoints, \
-         h5py.File(feature_dir / "descriptors.h5", mode="r") as f_descriptors, \
-         h5py.File(feature_dir / "matches.h5", mode="w") as f_matches:
-        
+    matcher = LightGlue(features='aliked').eval().to(device)
+
+    features = torch.load('features.pt')
+    
+    with h5py.File(feature_dir / "matches.h5", mode="w") as f_matches:
             for idx1, idx2 in tqdm(index_pairs, desc="Computing keypoing distances"):
+                
                 key1, key2 = paths[idx1].name, paths[idx2].name
 
-                matches = matcher.knnMatch(f_descriptors[key1][...], 
-                                   f_descriptors[key2][...], k=2)
-    
-                queryIdx, trainIdx = [], []
-            
-                for m,n in matches:
-                    if m.distance < 0.85*n.distance:
-                        queryIdx.append(m.queryIdx), trainIdx.append(m.trainIdx)
-                    
-                indices = np.column_stack((queryIdx, trainIdx))
-                # We have matches to consider
-                n_matches = len(trainIdx)
+                feats0, feats1  = features[key1], features[key2]
+                matches = matcher({'image0': feats0, 
+                                   'image1': feats1})
+
                 
+                feats0, feats1, matches = [rbd(x) for x in 
+                                             [feats0, feats1, matches]]
+                matches = matches['matches'].detach().cpu().numpy()
+                # We have matches to consider
+                n_matches = np.shape(matches)[0]
                 if n_matches:
                     if verbose:
                         print(f"{key1}-{key2}: {n_matches} matches")
                     # Store the matches in the group of one image
                     if n_matches >= min_matches:
                         group  = f_matches.require_group(key1)
-                        group.create_dataset(key2, data=indices)
+                        group.create_dataset(key2, data=matches)
+                        
 
 
 def import_into_colmap(
